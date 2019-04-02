133b3d45b20d5aa56726683525d144618e146f40
wzk <1215987050@qq.com>
2019-04-02 14:05:31 +0800

    首次提交到本地仓库

---
+++ src/com/itsxufe/line/StatusServlet.java
@@ -0,0 +1,29 @@
+package com.itsxufe.line;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class StatusServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//手动设置HTTP响应行中的状态码
+		response.setStatus(302);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
